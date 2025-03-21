c55592026efca71e766f70c7d67e096a21a979dc
kh277 <jkhcoding@naver.com>
2025-03-21 12:00:04 +0900

    Fix Point ratio Error

--- ConvexHull.py
+++ ConvexHull.py
@@ -1,13 +1,10 @@
-# 2차원 벡터의 외적값을 이용해 회전 방향 판별
 def CCW(a, b, c):
     # 양수(반시계), 0(일직선), 음수(시계)
     return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])
 
 
-# Convex Hull 구하기
-def convex_hull(points_X, points_Y):
-    
-    graph = [(points_Y[i], points_X[i]) for i in range(len(points_X))]
+def ConvexHull(pointX, pointY):
+    graph = [(pointY[i], pointX[i]) for i in range(len(pointX))]
     
     # x좌표 오름차순으로 정렬
     graph = sorted(set(graph))
@@ -30,7 +27,7 @@ def convex_hull(points_X, points_Y):
     
     # 아래쪽 Hull과 위쪽 Hull을 중복제거하여 합치기
     result = lower[:-1] + upper[:-1]
-    result_X = [result[i%len(result)][1] for i in range(len(result)+1)]
-    result_Y = [result[i%len(result)][0] for i in range(len(result)+1)]
+    resultX = [result[i%len(result)][1] for i in range(len(result)+1)]
+    resultY = [result[i%len(result)][0] for i in range(len(result)+1)]
     
-    return result_X, result_Y
+    return resultX, resultY
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import ConvexHull as ch
 
 
 # 기본 세팅
-def basic_settings():
+def init():
     # 페이지 기본 설정
     st.set_page_config(
         page_title="좌표 그리기",
@@ -26,84 +26,81 @@ def basic_settings():
     """, unsafe_allow_html=True)
     
 
-# UI 세팅
-def UserInterface():
+# 좌표 및 처리 옵션 반환
+def getOption():
     st.markdown("<div class='font25'>1. 좌표를 입력해주세요.", unsafe_allow_html=True)
 
     # 좌표 입력 필드
-    point_input = st.text_area("(ex: (0, 0), (2, 3)인 경우 0 0 2 3으로 입력.)")
-    
+    pointString = st.text_area("(ex: (0, 0), (2, 3)인 경우 0 0 2 3으로 입력.)")
+
     # 옵션 선택 필드
     outline = st.radio('윤곽선 선택', ['외곽선 미표시', '외곽선 표시', '볼록 껍질'])
     visual = st.button("시각화")
-    
-    
-    return [point_input, [outline, visual]]
+
+    return [pointString, [outline, visual]]
 
 
 # 점이 올바르게 입력되었는지 확인
-def check_point(point: list):
+def checkPoint(point: list):
     # 점 체크
     if len(point) > 0:
         temp = list(map(int, point.split()))
-        
-        # 짝수개 입력 체크
+
+        # 입력 좌표 수가 짝수인지 체크
         if len(temp) % 2 == 1 or len(temp) == 0:
             st.write("좌표 개수를 짝수로 입력해주세요.")
             return [False, [], []]
         else:
-            points_X = []
-            points_Y = []
+            pointX = []
+            pointY = []
             for i in range(len(temp)//2):
-                points_X.append(temp[i*2])
-                points_Y.append(temp[i*2+1])
+                pointX.append(temp[i*2])
+                pointY.append(temp[i*2+1])
 
-            return [True, points_X, points_Y]
-    
+            return [True, pointX, pointY]
     else:
         st.write("좌표를 입력해주세요.")
         return [False, [], []]
 
 
-def visualize(points_X: list, points_Y: list, option: list):
-    max_value = max(max(points_X), max(points_Y))
-    
+def visualize(pointX, pointY, option: list):
+    minX, maxX = min(pointX), max(pointX)
+    minY, maxY = min(pointY), max(pointY)
+
+    # 그래프 기본 설정
     plt.figure(figsize=(10, 10))
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.xticks(range(-1*max_value-3, max_value+3, 1))  # x축 눈금 설정
-    plt.yticks(range(-1*max_value-3, max_value+3, 1))  # y축 눈금 설정
-    plt.grid(color = 'gray', linestyle = '--', linewidth = 1)  # 그리드 설정
-    plt.xlim(-1*max_value-3, max_value+3)  # x축 범위 설정
-    plt.ylim(-1*max_value-3, max_value+3)  # y축 범위 설정
-    
-    # 윤곽선
+    marginX = (maxX-minX) * 0.1
+    marginY = (maxY-minY) * 0.1
+    plt.xlim(minX - marginX, maxX + marginX)
+    plt.ylim(minY - marginY, maxY + marginY)
+    plt.grid(color='gray', linestyle='--', linewidth=1)
+
+    # 옵션에 따른 좌표 출력
     if option[0] == '외곽선 미표시':
-        print("외곽선 미표시")
-        plt.plot(points_X, points_Y, marker='o', linestyle='', color='red')  # 점과 선 그리기
+        plt.plot(pointX, pointY, marker='o', linestyle='', color='red')
     elif option[0] == '외곽선 표시':
-        plt.plot(points_X, points_Y, marker='o', linestyle='-', color='red')  # 점과 선 그리기
+        plt.plot(pointX, pointY, marker='o', linestyle='-', color='red')
     elif option[0] == '볼록 껍질':
-        hull_X, hull_Y = ch.convex_hull(points_X, points_Y)
-        plt.plot(points_X, points_Y, marker='o', linestyle='', color='red')  # 점과 선 그리기
-        plt.plot(hull_X, hull_Y, marker='o', linestyle='-', color='blue')  # 점과 선 그리기
-    
-    # 그래프 표시
+        hullX, hullY = ch.ConvexHull(pointX, pointY)
+        plt.plot(pointX, pointY, marker='o', linestyle='', color='red')
+        plt.plot(hullX, hullY, marker='o', linestyle='-', color='blue')
+
     st.pyplot(plt)
-    
-    
+
+
 def main():
     # 1. 기본 세팅
-    basic_settings()
-    
+    init()
+
     # 2. 점 입력
-    point, option = UserInterface()
-    
+    pointString, option = getOption()
+
     # 3. 점이 올바르게 입력되었다면 그래프 시각화
-    isRight, points_X, points_Y = check_point(point)
+    isRight, pointX, pointY = checkPoint(pointString)
     if isRight == True:
-        visualize(points_X, points_Y, option)
-    
+        visualize(pointX, pointY, option)
 
 
 main()
